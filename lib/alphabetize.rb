5699f7ce6b8cf43979e5650816207213a2a5f6eb
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-20 17:22:32 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,10 +1,3 @@
 def alphabetize(arr)
-  arr.sort_by! do |a|
-    positions = []
-    a.each_char do |char|
-      positions << ESPERANTO_ALPHABET.index(char)
-    end
-    positions
-  end
-  arr
+  
 end
